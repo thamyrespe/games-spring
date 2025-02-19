4eefd6d3ad76a97a9940b28193d00253915d8a62
thamyres moraes <piresdemoraesthamyres@gmail.com>
2025-02-19 17:59:22 +0000

    salvando codigo web list.jsp

--- app/src/main/java/application/controller/CategoriaController.java
+++ app/src/main/java/application/controller/CategoriaController.java
@@ -20,5 +20,75 @@ public class CategoriaController {
 
     @RequestMapping("/list")
     public String list(Model ui) {
-        ui.addAtribute("categorias,")
+        ui.addAtribute("categorias", categoriaRepo.findALL());
+        return "categoria/list";
     }
+
+    @RequestMapping("/insert")
+    public String insert() {
+        return "categoria/insert";
+    } 
+
+    @RequestMapping(value= "/insert", method = RequeMethod.POST)
+    public String insert(@RequestParam("nome") String nome) {
+    Categorias categoria = new Categoria();
+    categoria.setNome(nome);
+
+    categoriaRepo.save(categoria);
+
+    return "redirect:/categoria/list";
+ }
+
+@RequestMapping("/update")
+public String update(
+    @RequestParam("id") long id, 
+    Model ui) {
+
+        Optional<Categoria> categoria = categoriaRepo.findById(id);
+
+        if(categoria.isPresent()) {
+            ui.addAtribute("categoria", categoria.get());
+            return "categoria/update";
+        }
+
+        return "redirect:/categoria/list";
+}
+
+@RequestMapping(value = "/update", method = RequeMethod.POST)
+public String update(
+    @RequestParam("id") long id, 
+    @RequestParam("nome") String nome ) {
+
+    Optional<Categoria> categoria = categoriaRepo.findById(id);
+
+    if(categoria.isPresent()) {
+        categoria.get().setNome(nome);
+
+        categoriaRepo.save(categoria.get());
+    } 
+
+    return "redirect:/categoria/list";
+}
+
+@RequestMapping("/delete")
+public String delete(
+    @RequestParam("id") long id,
+    Model ui) {
+
+        Optional<Categoria> categoria = categoriaRepo.findById(id);
+
+        if(categoria.isPresent()) {
+            ui.addAtribute("categoria", categoria.get());
+            return "categoria/delete";
+        }
+
+        return "redirect:/categoria/list";
+    }
+
+@RequestMapping(value = "/delete", method = RequeMethod.POST)
+public String delete(@RequestParam("id") long id) {
+    categoriaRepo.deleteById(id);
+
+    return "redirect:/categoria/list";
+  }       
+}
